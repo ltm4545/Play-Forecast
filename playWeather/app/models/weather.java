dc439a19e90a82d945f42bb359194ffece8ca757
ltm4545 <tianmaolan@192.168.1.9>
2016-03-27 23:32:28 +0800

    Demo Play-Forecast
    First time working on Play-Framewoek

---
+++ playWeather/app/models/weather.java
@@ -0,0 +1,19 @@
+package models;
+
+
+//import com.sun.tools.internal.ws.processor.model.Model;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by tianmaolan on 16/3/27.
+ */
+
+public class weather {
+
+
+        public String cityName;
+
+
+}
